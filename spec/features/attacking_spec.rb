4dbc27a7a2b4fd78f94fa762df1d9a3f9961a4e5
Alex <alex.satur89@gmail.com>
2017-10-12 08:33:29 +0100

    refactor with player class

--- spec/features/attacking_spec.rb
+++ spec/features/attacking_spec.rb
@@ -10,4 +10,15 @@ feature 'Attacking opponent' do
     click_button('Attack')
     expect(page).to have_content "Celery has 90 butter points"
   end
+
+  scenario 'returns to homescreen after an attack' do
+    sign_in_and_play
+    click_button('Attack')
+    click_button('Home')
+    expect(page).to have_content "Welcome Orange and Celery!"
+    expect(page).to have_content "Celery has 90 butter points"
+    expect(page).to have_content "Orange has 100 butter points"
+
+
+  end
 end
