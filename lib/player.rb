c9f7007e7242a48d370c4d97c5eac7488073d43e
Alex <alex.satur89@gmail.com>
2017-10-12 07:57:45 +0100

    player class tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,21 @@
 class Player
 
-  attr_reader :name
 
-  def initialize(name)
+  DEFAULT_HIT_POINTS = 100
+  attr_reader :name, :hit_points
+
+
+  def initialize(name, hit_points = DEFAULT_HIT_POINTS )
     @name = name
+    @hit_points = hit_points
   end
-  
+
+  def attack(player)
+    player.reduce_hit_points
+  end
+
+  def reduce_hit_points
+    @hit_points -= 10
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require './lib/player'
 
 describe Player do
   subject(:owliver) { Player.new('owliver')}
+  subject(:barry) { Player.new('barry')}
 
   describe '#name' do
     it 'returns the name' do
       expect(owliver.name).to eq 'owliver'
+    end
+  end
+
+  describe '#hit points' do
+    it 'returns the hit points' do
+      expect(owliver.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'one player can attack another' do
+      owliver.attack(barry)
+      expect(barry.hit_points).to eq 90
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { owliver.reduce_hit_points }.to change { owliver.hit_points }.by(-10)
     end
   end
 end
